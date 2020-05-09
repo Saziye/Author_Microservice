863a2f9327481a8bd2ebe5274662d0f1147a9863
Saz <saziye@gmail.com>
2020-05-10 00:30:41 +0300

    Implement func of author microservice

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Author;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthorController extends Controller
 {
@@ -25,7 +27,10 @@ class AuthorController extends Controller
      */
     public function index() 
     {
+        $authors = Author::all();
 
+        //return $authors;
+        return $this->successResponse($authors);
     }
 
     /**
@@ -34,7 +39,14 @@ class AuthorController extends Controller
      */
     public function store(Request $request) 
     {
-
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|max:255|in:male,female',
+            'country' => 'required|max:255',
+        ];
+        $this->validate($request, $rules);
+        $author = Author::create($request->all());
+        return $this->successResponse($author, Response::HTTP_CREATED);
     }
 
     /**
@@ -43,7 +55,8 @@ class AuthorController extends Controller
      */
     public function show($author) 
     {
-
+        $author = Author::findOrFail($author);
+        return $this->successResponse($author);
     }
     
     /**
@@ -52,7 +65,19 @@ class AuthorController extends Controller
      */
     public function update(Request $request, $author) 
     {
-
+        $rules = [
+            'name' => 'max:255',
+            'gender' => 'max:255|in:male,female',
+            'country' => 'max:255',
+        ];
+        $this->validate($request, $rules);
+        $author = Author::findorFail($author);
+        $author->fill($request->all());
+        if ($author->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $author->save();
+        return $this->successResponse($author);
     }
 
     /**
@@ -61,6 +86,8 @@ class AuthorController extends Controller
      */
     public function destroy($author) 
     {
-
+        $author = Author::findorFail($author);
+        $author->delete();
+        return $this-> successResponse($author);
     }
 }
